6a51bd65ed6de4cbb5b67283b763f9aa38a7d0f8
m4ujo <mauriciocast11@outlook.com>
2022-12-24 10:10:28 -0500

    Add validation for tax specific attributes

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -7,11 +7,22 @@ sap.ui.define(
   function (Controller, JSONModel, MessageBox) {
     'use strict'
 
-    const domain = 'https://usalspgsa660.na.pg.com:8443'
-    const rSpacesPattern = /\s/g
-    const rPatternLetters = /[^\d]/g
+    const DOMAIN = 'https://usalspgsa660.na.pg.com:8443'
+    const REGEX_SPACES = /\s/g
+    const REGEX_LETTERS = /[^\d]/g
+    const URL_CODES = [
+      'GET_TAX_COUNTRY_LIST',
+      'GET_TAX_TYPES_LIST',
+      'GET_TAX_SCENARIOS',
+      'GET_TAX_FIELDSTATUS',
+      'GET_TAXJUR_LIST',
+      'GET_SAP_COUNTRY_LIST',
+      'GET_EUCODE_LIST',
+      'GET_TAX_PROCEDURE'
+    ]
 
     let data = []
+    let url = ''
 
     return Controller.extend('taco.controller.App', {
       onInit: function () {
@@ -26,7 +37,9 @@ sap.ui.define(
         this.oCbxVatScenarios = this.byId('cbx-vatScenarios')
         // Step 3
         this.oIptTaxCodeId = this.byId('input-taxCodeId')
+        this.oIptTaxName = this.byId('input-taxName')
         this.oChkCheckId = this.byId('chk-checkId')
+        this.oCbxTaxJur = this.byId('cbx-taxJur')
         this.oCbxReportCountry = this.byId('cbx-reportCountry')
         this.oCbxEuCode = this.byId('cbx-euCode')
         this.oCbxTargetTaxCode = this.byId('cbx-targetTaxCode')
@@ -34,22 +47,22 @@ sap.ui.define(
         // Step 4
         this.oTableRevTaxRates = this.byId('table-revTaxRates')
 
-        this.getDataJSON(
-          this.buildUrl('GET_TAX_COUNTRY_LIST'),
-          this.oCbxCountryKey
-        )
-        this.getDataJSON(this.buildUrl('GET_TAX_TYPES_LIST'), this.oCbxTaxType)
+        url = this.buildUrl(URL_CODES[0])
+        this.getDataJSON(url, this.oCbxCountryKey)
+
+        url = this.buildUrl(URL_CODES[1])
+        this.getDataJSON(url, this.oCbxTaxType)
       },
 
       // Utils
       validateNumericInput: function (oEvent) {
         const _oInput = oEvent.getSource()
         let val = _oInput.getValue()
-        val = val.replace(rPatternLetters, '')
+        val = val.replace(REGEX_LETTERS, '')
         _oInput.setValue(val)
       },
       buildUrl: function (code, ...params) {
-        let url = `${domain}/pg_itx?sap-client=400&code=${code}`
+        let url = `${DOMAIN}/pg_itx?sap-client=400&code=${code}`
         if (params) {
           params.forEach((param) => {
             url += `&${param.type}=${param.value}`
@@ -60,7 +73,7 @@ sap.ui.define(
       getDataJSON: function (url, ...obj) {
         const fSetData = this.setData
         const fDataNotFound = this.dataNotFound
-        jQuery.ajax({
+        $.ajax({
           url,
           crossDomain: true,
           method: 'GET',
@@ -87,22 +100,22 @@ sap.ui.define(
         }
       },
 
-      // Tax Properties - Step 1
+      // Tax Properties - Step 2
       validateTaxProperties: function () {
         const sCountryKey = this.oCbxCountryKey
           .getValue()
-          .replace(rSpacesPattern, '')
+          .replace(REGEX_SPACES, '')
         const sTaxType = this.oCbxTaxType.getSelectedKey()
 
         this.oWizard.invalidateStep(this.byId('step-taxProperties'))
 
         if (sCountryKey !== '' && sTaxType !== '') {
-          const taxScenUrl = this.buildUrl(
-            'GET_TAX_SCENARIOS',
+          url = this.buildUrl(
+            URL_CODES[2],
             { type: 'land1', value: sCountryKey },
             { type: 'mwart', value: sTaxType }
           )
-          this.getDataJSON(taxScenUrl)
+          this.getDataJSON(url)
         }
 
         setTimeout(() => {
@@ -129,40 +142,38 @@ sap.ui.define(
         data = []
       },
       backToStep1: function () {
-        const taxScenUrl = this.buildUrl(
-          'GET_TAX_SCENARIOS',
+        url = this.buildUrl(
+          URL_CODES[2],
           { type: 'land1', value: this.oCbxCountryKey.getValue() },
           { type: 'mwart', value: this.oCbxTaxType.getSelectedKey() }
         )
-        this.getDataJSON(taxScenUrl)
+        this.getDataJSON(url)
+        this.oCbxVatScenarios.setModel(new JSONModel())
         this.oCbxCountryKey.setEnabled(true)
         this.oCbxTaxType.setEnabled(true)
         this.oWizard.previousStep()
-        this.oCbxVatScenarios.setModel(new JSONModel())
       },
 
-      // Vat Scenario - Step 2
+      // Vat Scenario - Step 3
       validateVatScenario: function () {
         this.oWizard.invalidateStep(this.byId('step-vatScenarios'))
         const sVatScenario = this.oCbxVatScenarios
           .getValue()
-          .replace(rSpacesPattern, '')
+          .replace(REGEX_SPACES, '')
 
         if (sVatScenario !== '') {
-          const taxFieldStatusUrl = this.buildUrl(
-            'GET_TAX_FIELDSTATUS',
+          url = this.buildUrl(
+            URL_CODES[3],
             { type: 'land1', value: this.oCbxCountryKey.getValue() },
             { type: 'mwart', value: this.oCbxTaxType.getSelectedKey() },
             { type: 'scen', value: sVatScenario }
           )
-          this.getDataJSON(taxFieldStatusUrl)
+          this.getDataJSON(url)
         }
 
         setTimeout(() => {
           if (data.length === 0) {
-            MessageBox.error(
-              'No data found, for the entered scenario.'
-            )
+            MessageBox.error('No data found, for the entered scenario.')
             this.oWizard.invalidateStep(this.byId('step-vatScenarios'))
           } else {
             this.oWizard.validateStep(this.byId('step-vatScenarios'))
@@ -170,8 +181,48 @@ sap.ui.define(
         }, 1500)
       },
       completeVatScenarios: function () {
-        data.forEach(x => console.log(x))
+        console.log(data)
+        data.forEach((x) => {
+          const field = this.byId(`field-${x.fieldname}`)
+          field.setVisible(true)
+          if (x.fieldname === 'CHECKID' && x.default_value === 'X') {
+            this.oChkCheckId.setSelected(true)
+          }
+          if (x.scr_status === 'R') {
+            const inputProperties = field.mAggregations.content[1].mProperties
+            inputProperties.required = true
+          } else if (x.scr_status === 'O') {
+            console.log('')
+          } else if (x.scr_status === 'S') {
+            field.setVisible(false)
+          }
+        })
+        this.oCbxVatScenarios.setEnabled(false)
+
+        data = []
+        url = this.buildUrl(URL_CODES[4], {
+          type: 'land1',
+          value: this.oCbxCountryKey.getValue()
+        })
+        this.getDataJSON(url, this.oCbxTaxJur)
+
+        data = []
+        url = this.buildUrl(URL_CODES[5])
+        this.getDataJSON(url, this.oCbxReportCountry)
+
+        data = []
+        url = this.buildUrl(URL_CODES[6])
+        this.getDataJSON(url, this.oCbxEuCode)
       },
+      backToStep2: function () {
+        this.oCbxVatScenarios.setEnabled(true)
+        this.oWizard.previousStep()
+      },
+
+      // Vat Scenario - Step 4
+      validateTaxAttr: function () {},
+      completeTaxAttr: function () {},
+      backToStep3: function () {},
 
       onComplete: function () {
         console.log('Terminado')
